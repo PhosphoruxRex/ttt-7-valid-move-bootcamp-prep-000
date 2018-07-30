0df4fdab70c8cf4527fd99c9a10fcb9ec8e583d3
Lung Chen <lungheichen@gmail.com>
2018-07-30 02:29:36 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, num)
-  if num.between(0, 8) && position_taken(board, num)
+  if num.between?(0, 8) && position_taken(board, num)
     return true
   end
 end
